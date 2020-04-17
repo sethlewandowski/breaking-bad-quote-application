86cc539f9fd762e8e2adfecadae4c0ecad006e2f
sethl <lewandowski.seth@gmail.com>
2020-04-17 16:38:45 -0700

    Updates

---
+++ precache-manifest.9dfd0c14db68557bdb1d5c6e425e5206.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0dc9b96368780b24769572faba64241",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "0541c155ad0da956d019",
+    "url": "/random-quote-machine/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "55c0a818445b579b2408",
+    "url": "/random-quote-machine/static/css/main.c6b592b5.chunk.css"
+  },
+  {
+    "revision": "0541c155ad0da956d019",
+    "url": "/random-quote-machine/static/js/2.61d6b29e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.61d6b29e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55c0a818445b579b2408",
+    "url": "/random-quote-machine/static/js/main.178e9720.chunk.js"
+  },
+  {
+    "revision": "52b3aacfd4b50869dda1",
+    "url": "/random-quote-machine/static/js/runtime-main.116f0ca4.js"
+  },
+  {
+    "revision": "3585c5fe513d1047c757c3293f29cd95",
+    "url": "/random-quote-machine/static/media/background.3585c5fe.jpg"
+  }
+]);
